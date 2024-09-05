49bea5a658b6f0da691fddf594a449bf15ec225a
momingel <68225848+momingel@users.noreply.github.com>
2024-09-06 01:39:07 +0800

    Update live2d.js

--- live2d.js
+++ live2d.js
@@ -28,7 +28,7 @@ if (screen.width >= 768) {
 	Promise.all([
 		loadExternalResource(live2d_path + "waifu.css", "css"),
 		loadExternalResource(live2d_path + "live2d.min.js", "js"),
-		loadExternalResource(live2d_path + "waifu-tips.js", "js")
+		loadExternalResource("/waifu-tips.js", "js")
 	]).then(() => {
 		// 配置选项的具体用法见 README.md
 		initWidget({
